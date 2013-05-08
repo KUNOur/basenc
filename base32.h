2ce03ad3832562a70ea280223493ffcb6dc64857
David Jackson <david@dwjackson.ca>
2013-05-08 12:37:12 -0400

    Add Base32 encoding array to header

--- base32.h
+++ base32.h
@@ -16,6 +16,18 @@
 typedef unsigned char base32_byte;
 
 /**
+ * \var char base32_encoding
+ * \brief The encoding array for Base32
+ *
+ * The Base32 alphabet maps the numebers 0 to 31 to uppercase Latin characters
+ * and numbers between 2 and 7.
+ */
+char base32_encoding[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
+			   'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+			   'U', 'V', 'W', 'X', 'Y', 'Z', '2', '3', '4', '5',
+			   '6', '7' };
+
+/**
  * Base32 Encode
  *
  * Given a bytestring of data, find the Base32 encoding of that bytestring. It
