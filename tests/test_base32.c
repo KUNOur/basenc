3aeb7133b627e6f6c105ba1f7a6237076092f934
David Jackson <david@dwjackson.ca>
2013-05-08 14:56:20 -0400

    Add a Check unit test for Base32 encoding

---
+++ tests/test_base32.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <string.h>
+#include <check.h>
+
+#include "base32.h"
+
+START_TEST (test_base32)
+{
+  char str[] = "This is a test.";
+  char correct_encoded[] = "KRUGS4ZANFZSAYJAORSXG5BO";
+
+  char *encoded = NULL;
+  encoded = malloc(strlen(str) * 2 + 1);
+
+  int ret = base32_encode(str, strlen(str), encoded);
+
+  ck_assert_int_eq(ret, 1);
+  ck_assert_str_eq (encoded, correct_encoded);
+
+  free(encoded);
+}
+END_TEST
+
+Suite*
+base32_suite(void)
+{
+  Suite* s = suite_create("Base32");
+
+  TCase *tc_core = tcase_create("Core");
+  tcase_add_test (tc_core, test_base32);
+  suite_add_tcase(s, tc_core);
+
+  return s;
+}
+
+int
+main(void)
+{
+  int number_failed;
+  Suite *s = base32_suite();
+  SRunner *sr = srunner_create(s);
+  srunner_run_all(sr, CK_NORMAL);
+  number_failed = srunner_ntests_failed(sr);
+  srunner_free(sr);
+  return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
