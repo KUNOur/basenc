2154f26ddee63c3a6d88964555ed182e9ccea9bf
David Jackson <david@dwjackson.ca>
2013-05-09 23:15:18 -0400

    Change base32_encode to use base32_encode_block

--- src/base32.c
+++ src/base32.c
@@ -48,6 +48,8 @@ base32_encode(const base32_byte *data, int len, char* enc)
     j = 0;
     /* Encode first part of the data */
     for (i = 0; i + 5 < len; i += 5) {
+      base32_encode_block(data[i], enc[j]);
+      /*
       enc_index[0] = (data[i] >> 3) & 0x1F;
       enc_index[1] = ((data[i] << 2) & 0x1C) | ((data[i+1] >> 6) & 0x3);
       enc_index[2] = (data[i+1] >> 1) & 0x1F;
@@ -56,6 +58,7 @@ base32_encode(const base32_byte *data, int len, char* enc)
       enc_index[5] = (data[i+3] >> 2) & 0x1F;
       enc_index[6] = ((data[i+3] << 3) & 0x18) | ((data[i+4] >> 5) & 0x7);
       enc_index[7] = data[i+4] & 0x1F;
+      */
 
       for (k = 0; k < 8; k++) {
 	enc[j+k] = base32_encoding[enc_index[k]];
