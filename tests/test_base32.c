76d0dffdaba305fa43c08a522fb1181c9ae77cb8
David Jackson <david@dwjackson.ca>
2013-05-10 15:53:55 -0400

    Fix up the base32_decode_block test

--- tests/test_base32.c
+++ tests/test_base32.c
@@ -61,6 +61,7 @@ base32_suite(void)
   TCase *tc_core = tcase_create("Core");
   tcase_add_test (tc_core, test_base32_encode_block);
   tcase_add_test (tc_core, test_base32);
+  tcase_add_test (tc_core, test_base32_decode_block);
   suite_add_tcase(s, tc_core);
 
   return s;
