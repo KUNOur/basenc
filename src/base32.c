66e417e8e42f6190c731062c13462918c3555542
David Jackson <david@dwjackson.ca>
2013-05-13 12:18:01 -0400

    Encode data less than 5B, remove useless comments

--- src/base32.c
+++ src/base32.c
@@ -96,31 +96,9 @@ base32_encode(const base32_byte *data, int len, char* enc)
     /* Encode first part of the data */
     for (i = 0; i + 5 <= len; i += 5) {
       base32_encode_block(&data[i], &enc[j]);
-      /*
-      enc_index[0] = (data[i] >> 3) & 0x1F;
-      enc_index[1] = ((data[i] << 2) & 0x1C) | ((data[i+1] >> 6) & 0x3);
-      enc_index[2] = (data[i+1] >> 1) & 0x1F;
-      enc_index[3] = ((data[i+1] << 4) & 0x10) | ((data[i+2] >> 4) & 0xF);
-      enc_index[4] = ((data[i+2] << 1) & 0x1E) | ((data[i+3] >> 7) & 0x1);
-      enc_index[5] = (data[i+3] >> 2) & 0x1F;
-      enc_index[6] = ((data[i+3] << 3) & 0x18) | ((data[i+4] >> 5) & 0x7);
-      enc_index[7] = data[i+4] & 0x1F;
-      */ 
-
-      /*
-      for (k = 0; k < 8; k++) {
-	enc[j+k] = base32_encoding[enc_index[k]];
-      }
-      */
-
       j += 8;
     }
     remaining = len - i;
-    /*
-    printf("[DEBUG] len = %d\n", len);
-    printf("[DEBUG] i = %d\n", i);
-    printf("[DEBUG] remaining = %d\n", remaining);
-    */
     if (remaining > 0) {
       for (k = 0; k < remaining; k++) {
 	final_block[k] = data[i+k];
@@ -133,7 +111,8 @@ base32_encode(const base32_byte *data, int len, char* enc)
 
     success = 1;
   } else if (len > 0 && len < 5) {
-    /* TODO: Encode data of a length less than 5, have to padd data */
+    /* Encode data of a length less than 5 */
+    base32_encode_small_block(data, len, enc);
   } else {
     success = 0;
   }
